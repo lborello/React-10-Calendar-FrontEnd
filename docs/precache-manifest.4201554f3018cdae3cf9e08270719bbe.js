24949520d85d286af5fef79812a3a1714cd5faf0
lborello <luis_borello@hotmail.com>
2021-05-03 20:00:36 -0300

    ff
    ff

---
+++ docs/precache-manifest.4201554f3018cdae3cf9e08270719bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df5187e3ed2a318fe9257cec9b4da72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6637eeebb453c4f642",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "bc06475d656881241b9d",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2c6637eeebb453c4f642",
+    "url": "/static/js/2.4bdbacea.chunk.js"
+  },
+  {
+    "revision": "f3559d6e50db38fb054438bd1fc6d856",
+    "url": "/static/js/2.4bdbacea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc06475d656881241b9d",
+    "url": "/static/js/main.52115ab0.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);

dc2b9c5637ee4f5b9c415f826267dc731a82e77d
lborello <luis_borello@hotmail.com>
2021-05-03 19:39:29 -0300

    dsa
    as

---
+++ docs/precache-manifest.bc76ec410a02be2f36b6a81828a8c0bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f0e81ecf5ebe338f3c7bb641fb1105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6637eeebb453c4f642",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "fa8d9612379c1a77ca89",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2c6637eeebb453c4f642",
+    "url": "/static/js/2.4bdbacea.chunk.js"
+  },
+  {
+    "revision": "f3559d6e50db38fb054438bd1fc6d856",
+    "url": "/static/js/2.4bdbacea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8d9612379c1a77ca89",
+    "url": "/static/js/main.56daaeff.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
